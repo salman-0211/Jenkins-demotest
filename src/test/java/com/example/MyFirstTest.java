14da3191836b6a414852452df676b6aa6a4de7d8
salman-0211 <babasalman342@gmail.com>
2025-09-02 15:43:27 +0530

    Add MyFirstTest unit test

---
+++ src/test/java/com/example/MyFirstTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MyFirstTest {
+    @Test
+    public void testAddition() {
+        assertEquals(2, 1 + 1);
+    }
+}
+
